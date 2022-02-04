b59f79845cff11b3258675ddab4959384fc69208
Shir Pinchasi <shirpi9999@gmail.com>
2022-02-04 23:43:39 +0200

    removed cors

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ app.use(cookieParser());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["https://crm-ui.herokuapp.com","http://localhost:3000/"],
+  origin: ["https://crm-ui.herokuapp.com","http://localhost:3000"],
   credentials: true
 }));
 
-app.options("*",cors())
 
 app.use(users,calls,systems,cors())
 
