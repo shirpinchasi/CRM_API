3a5b32f005bcc5bb28628796515f689f81915862
Shir Pinchasi <shirpi9999@gmail.com>
2022-02-05 15:50:27 +0200

    added header

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -27,6 +27,7 @@ app.use(function(req,res,next){
   res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept",
+      'Access-Control-Allow-Origin', '*'
   );
   next();
 });
