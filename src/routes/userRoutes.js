313c88ad812beb12c01603cd0dca2d5fd9e73c69
Shir Pinchasi <shirpi9999@gmail.com>
2022-02-05 15:27:41 +0200

    removed headers

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -27,7 +27,6 @@ app.use(function(req,res,next){
   res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept",
-      'Access-Control-Allow-Origin', '*'
   );
   next();
 });
