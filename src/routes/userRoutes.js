3551549ecf6cc19b9b97c9dfd04cc6c632bbb91f
Shir Pinchasi <shirpi9999@gmail.com>
2022-02-05 12:55:41 +0200

    removed urlencoded

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser")
-app.use(express.urlencoded({extended:false}));
+
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const cookieParser = require('cookie-parser')
 const jwt = require("jsonwebtoken");
@@ -31,7 +31,8 @@ app.use(function(req,res,next){
 app.post("/user/signup",verifySignUp.checkDuplicateUserNameOrEmail,verifySignUp.checkRolesExisted,(req,res)=>{
   const user = new User(req.body);
   user.email = req.body.email
-    user.password = crypto.createHash("SHA256").update(user.password).digest("hex");
+  user.password = crypto.createHash("SHA256").update(user.password).digest("hex");
+    
   user.save((err,user)=>{
     if(err){
       res.status(500).send({ message: err + "error saving user" });
